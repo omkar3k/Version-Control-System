d30616b469611380c59ba2debc0cf207fb4bdafd
omkar3k <omkar108ok@gmail.com>
2025-02-25 13:40:51 +0530

    main

---
+++ helo/Tries.java
@@ -0,0 +1,15 @@
+/*
+ * git add
+ * git commit -m "message"
+ * git push <remote repo> <branch>
+ * 
+ * git add
+ * git commit -m "added tries"
+ * git push origin main
+ */
+
+public class Tries {
+    
+    
+    
+}
